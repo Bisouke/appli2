cc6b0dc36fbd08b4d1dea6522c37136c7e59157b
brice <bricejoachim.ba@gmail.com>
2025-05-08 21:33:57 +0200

    tri historique pleins croissant décroissant fonctionnel

--- app/src/main/java/com/example/appli2/ActivityHistorique2.java
+++ app/src/main/java/com/example/appli2/ActivityHistorique2.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,6 +12,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -32,11 +34,10 @@ public class ActivityHistorique2 extends AppCompatActivity {
 
     private Button buttonSupprimer;
     private TextView textViewNbPlein;
-    private Spinner spinnerTriHistorique;
-    LinearLayout linearLayoutHistorique, itemLinearLayout;
-    GasDataCollection gasdataCollec;
-    @Nullable
-    View selected_view;
+    private ToggleButton toggleButton_tri;
+    private LinearLayout linearLayoutHistorique, itemLinearLayout;
+    private GasDataCollection gasdataCollec;
+    private View selected_view;
 
     // View list containing each inflated views
     private List<View> inflated_view_list;
@@ -58,8 +59,8 @@ public class ActivityHistorique2 extends AppCompatActivity {
             return insets;
         });
 
-        // Spinner
-        spinnerTriHistorique = findViewById(R.id.spinnerTriHistorique);
+        // Toggle button
+        toggleButton_tri = findViewById(R.id.toggleButton_tri);
         // Button
         buttonSupprimer = findViewById(R.id.buttonSupprimerHistorique);
         // TextView
@@ -77,159 +78,8 @@ public class ActivityHistorique2 extends AppCompatActivity {
 
 
 
-        View.OnClickListener listener = new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
 
-//                int color, alpha = 0;
-//
-//                // get background color
-//                Drawable background = v.getBackground();
-//
-//                if (background instanceof ColorDrawable) {
-//                    // convert Drawable to ColorDrawable
-//                    color = ((ColorDrawable) background).getColor();
-//                    // get alpha channel
-//                    alpha = Color.alpha(color);
-//                }
-
-                // remove background color for each inflated view
-                for (View view : inflated_view_list)
-                {
-                    // alpha to 0 : invisible color
-                    view.setBackgroundColor(Color.argb(0,149,183,253));
-                }
-
-                // set background color on the clicked view
-                v.setBackgroundColor(Color.argb(127,149,183,253));
-
-                // change selected view
-                selected_view = v;
-
-                // enable delete button
-                buttonSupprimer.setEnabled(true);
-            }
-        };
-
-
-        //          INFLATE NEW VIEW IN LINEAR LAYOUT
-
-        // create the inflater
-        LayoutInflater inflater = LayoutInflater.from(this);
-
-        // get the gas data list from GasDataCollection class
-        List<GasData> list = gasdataCollec.getAllGasData(GasDataCollection.EARLIEST_TOP);
-
-        inflated_view_list = new ArrayList<>();
-
-        // CREATE, ADD, MODIFY THE INFLATED VIEW
-        for (GasData gasdata : list)
-        // for each gas data
-        {
-            // inflate the view "item_historique_plein"
-            View inflated_view = inflater.inflate(R.layout.item_historique_plein,
-                    linearLayoutHistorique, false);
-
-            // add the view to the list view
-            inflated_view_list.add(inflated_view);
-
-            // FIND INNER VIEWS
-            // ligne 1
-            TextView textViewItemHist_ligne1 =
-                    inflated_view.findViewById(R.id.textViewItemHist_ligne1);
-            // ligne 2
-            TextView textViewItemHist_ligne2 =
-                    inflated_view.findViewById(R.id.textViewItemHist_ligne2);
-            // prix
-            TextView textViewItemHist_prix =
-                    inflated_view.findViewById(R.id.textViewItemHist_prix);
-            // temps
-            TextView textViewItemHist_temps =
-                    inflated_view.findViewById(R.id.textViewItemHist_temps);
-            // color
-            View textViewItemHist_color =
-                    inflated_view.findViewById(R.id.textViewItemHist_color);
-
-            // MODIFY INNER VIEWS VALUES
-
-            // volume + fuel
-            textViewItemHist_ligne1.setText(gasdata.getVolume() + "L de " + gasdata.getFuel());
-
-            // liter price + date
-            textViewItemHist_ligne2.setText(gasdata.getPrice_liter() + " €/L  "
-                    + gasdata.getDateStr());
-
-            // price
-            textViewItemHist_prix.setText(gasdata.getTotal_price() + " €");
-
-            // time elapsed
-            LocalDate current_time = LocalDate.now();
-            long daysBetween = ChronoUnit.DAYS.between(gasdata.getDate(), current_time);
-            long monthsBetween = ChronoUnit.MONTHS.between(gasdata.getDate(), current_time);
-            long yearsBetween = ChronoUnit.YEARS.between(gasdata.getDate(), current_time);
-
-            if (daysBetween == 0)
-            {
-                // use "today"
-                textViewItemHist_temps.setText("aujourd'hui");
-            }
-            else if (daysBetween <= 31)
-            {
-                // use days
-                textViewItemHist_temps.setText("il y a " + daysBetween + " jours");
-            }
-            else if (monthsBetween < 12)
-            {
-                // use months
-                textViewItemHist_temps.setText("il y a " + monthsBetween + " mois");
-            }
-            else
-            {
-                // use year
-                textViewItemHist_temps.setText("il y a " + yearsBetween + " ans");
-            }
-
-
-
-            // fuel color
-            switch (gasdata.getFuel())
-            {
-                case "SP98":
-                    textViewItemHist_color.setBackgroundColor(
-                            Color.parseColor("#44ba56")); // SP98 green
-                    break;
-
-                case "S95":
-                    textViewItemHist_color.setBackgroundColor(
-                            Color.parseColor("#44ba56")); // SP95 green
-                    break;
-
-                case "SP95E10":
-                    textViewItemHist_color.setBackgroundColor(
-                            Color.parseColor("#44ba56")); // SP95 green
-                    break;
-
-                case "E85":
-                    textViewItemHist_color.setBackgroundColor(
-                            Color.parseColor("#0170dd")); // E85 blue
-                    break;
-
-                case "DIESEL":
-                    textViewItemHist_color.setBackgroundColor(
-                            Color.parseColor("#fedc01")); // Diesel yellow
-                    break;
-            }
-
-            // add the view to the linear layout
-            linearLayoutHistorique.addView(inflated_view);
-
-            // set click listener
-            inflated_view.setOnClickListener(listener);
-
-        }
-
-
+        inflateGasDataViews();
 
 
                         /******************************************************
@@ -237,18 +87,37 @@ public class ActivityHistorique2 extends AppCompatActivity {
                         *******************************************************/
 
 
-        buttonSupprimer.setOnClickListener(v -> {
-            // deleteGasData();
-        });
+        buttonSupprimer.setOnClickListener(new View.OnClickListener()   {
+               @Override
+               public void onClick(View v)
+               {
+                   linearLayoutHistorique.removeAllViews();
+               }
+           });
 
-        spinnerTriHistorique.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+        toggleButton_tri.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            }
+            public void onClick(View v)
+            {
+                // get button state
+                boolean isSelected = toggleButton_tri.isSelected();
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+                // change background ressrouce
+                if (isSelected)
+                {
+                    toggleButton_tri.setBackgroundResource(R.drawable.up_arrow);
+                }
+                else
+                {
+                    toggleButton_tri.setBackgroundResource(R.drawable.down_arrow);
+                }
 
+                // reverse button state for next click
+                toggleButton_tri.setSelected(!isSelected);
+
+                // update gas data list view
+                inflateGasDataViews();
             }
         });
 
@@ -292,6 +161,174 @@ public class ActivityHistorique2 extends AppCompatActivity {
 //        listViewHistorique.setAdapter(adapter);
 //        // listViewHistorique.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 //        adapter.notifyDataSetChanged(); // update listView
+    }
+
+    public void inflateGasDataViews()
+    // INFLATE NEW VIEWS IN LINEAR LAYOUT
+    {
+
+        // clean all existing views inside the vertical layout
+        linearLayoutHistorique.removeAllViews();
+
+        // click listener on the whole view
+        View.OnClickListener listener = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+
+//                int color, alpha = 0;
+//
+//                // get background color
+//                Drawable background = v.getBackground();
+//
+//                if (background instanceof ColorDrawable) {
+//                    // convert Drawable to ColorDrawable
+//                    color = ((ColorDrawable) background).getColor();
+//                    // get alpha channel
+//                    alpha = Color.alpha(color);
+//                }
+
+                // remove background color for each inflated view
+                for (View view : inflated_view_list)
+                {
+                    // alpha to 0 : invisible color
+                    view.setBackgroundColor(Color.argb(0,149,183,253));
+                }
+
+                // set background color on the clicked view
+                v.setBackgroundColor(Color.argb(127,149,183,253));
+
+                // change selected view
+                selected_view = v;
+
+                // enable delete button
+                buttonSupprimer.setEnabled(true);
+            }
+        };
+
+        // get the gas data list from GasDataCollection class
+        List<GasData> gasDataList;
+
+        // get user sorting type (ascendant or descending)
+
+        if (toggleButton_tri.isSelected())
+        {
+            gasDataList = gasdataCollec.getAllGasData(GasDataCollection.EARLIEST_TOP);
+        }
+        else
+        {
+            gasDataList = gasdataCollec.getAllGasData(GasDataCollection.OLDEST_TOP);
+        }
+
+        // create the inflater
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        inflated_view_list = new ArrayList<>();
+
+        // CREATE, ADD, MODIFY THE INFLATED VIEW
+        for (GasData gasdata : gasDataList)
+        // for each gas data<
+        {
+            // inflate the view "item_historique_plein"
+            View inflated_view = inflater.inflate(R.layout.item_historique_plein,
+                    linearLayoutHistorique, false);
+
+            // add the view to the list view
+            inflated_view_list.add(inflated_view);
+
+            // find and modify inner views
+
+            // FIND INNER VIEWS
+            // ligne 1
+            TextView textViewItemHist_ligne1 =
+                    inflated_view.findViewById(R.id.textViewItemHist_ligne1);
+            // ligne 2
+            TextView textViewItemHist_ligne2 =
+                    inflated_view.findViewById(R.id.textViewItemHist_ligne2);
+            // prix
+            TextView textViewItemHist_prix =
+                    inflated_view.findViewById(R.id.textViewItemHist_prix);
+            // temps
+            TextView textViewItemHist_temps =
+                    inflated_view.findViewById(R.id.textViewItemHist_temps);
+            // color
+            View textViewItemHist_color =
+                    inflated_view.findViewById(R.id.textViewItemHist_color);
+
+            // MODIFY INNER VIEWS VALUES
+            // volume + fuel
+            textViewItemHist_ligne1.setText(gasdata.getVolume() + "L de " + gasdata.getFuel());
+
+            // liter price + date
+            textViewItemHist_ligne2.setText(gasdata.getPrice_liter() + " €/L  "
+                    + gasdata.getDateStr());
+
+            // price
+            textViewItemHist_prix.setText(gasdata.getTotal_price() + " €");
+
+            // time elapsed
+            LocalDate current_time = LocalDate.now();
+            long daysBetween = ChronoUnit.DAYS.between(gasdata.getDate(), current_time);
+            long monthsBetween = ChronoUnit.MONTHS.between(gasdata.getDate(), current_time);
+            long yearsBetween = ChronoUnit.YEARS.between(gasdata.getDate(), current_time);
+
+            if (daysBetween == 0)
+            {
+                // use "today"
+                textViewItemHist_temps.setText("aujourd'hui");
+            }
+            else if (daysBetween <= 31)
+            {
+                // use days
+                textViewItemHist_temps.setText("il y a " + daysBetween + " jours");
+            }
+            else if (monthsBetween < 12)
+            {
+                // use months
+                textViewItemHist_temps.setText("il y a " + monthsBetween + " mois");
+            }
+            else
+            {
+                // use year
+                textViewItemHist_temps.setText("il y a " + yearsBetween + " ans");
+            }
+
+            // fuel color
+            switch (gasdata.getFuel())
+            {
+                case "SP98":
+                    textViewItemHist_color.setBackgroundColor(
+                            Color.parseColor("#44ba56")); // SP98 green
+                    break;
+
+                case "S95":
+                    textViewItemHist_color.setBackgroundColor(
+                            Color.parseColor("#44ba56")); // SP95 green
+                    break;
+
+                case "SP95E10":
+                    textViewItemHist_color.setBackgroundColor(
+                            Color.parseColor("#44ba56")); // SP95 green
+                    break;
+
+                case "E85":
+                    textViewItemHist_color.setBackgroundColor(
+                            Color.parseColor("#0170dd")); // E85 blue
+                    break;
+
+                case "DIESEL":
+                    textViewItemHist_color.setBackgroundColor(
+                            Color.parseColor("#fedc01")); // Diesel yellow
+                    break;
+            }
+
+            // add the view to the linear layout
+            linearLayoutHistorique.addView(inflated_view);
+
+            // set click listener
+            inflated_view.setOnClickListener(listener);
+
+        }
     }
 
 
